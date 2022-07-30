49f4716226211aab738db5790d84456eb656ea2f
faroukhamadi <faroukhamadi1@outlook.com>
2022-07-31 00:22:15 +0100

    push for desktop

--- web/src/lib/components/home/main.svelte
+++ web/src/lib/components/home/main.svelte
@@ -1,1 +1,64 @@
 <input type="text" placeholder="Create Post" class="input input-bordered min-w-full max-w-xs" />
+<div class="post-container">
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Post Title</h2>
+			<p>
+				I don't even know what this post is about but I'll just keep typing until I fill this card
+			</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Upvote</button>
+				<button class="btn btn-primary">Downvote</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Post Title</h2>
+			<p>
+				I don't even know what this post is about but I'll just keep typing until I fill this card
+			</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Upvote</button>
+				<button class="btn btn-primary">Downvote</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Post Title</h2>
+			<p>
+				I don't even know what this post is about but I'll just keep typing until I fill this card
+			</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Upvote</button>
+				<button class="btn btn-primary">Downvote</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Post Title</h2>
+			<p>
+				I don't even know what this post is about but I'll just keep typing until I fill this card
+			</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Upvote</button>
+				<button class="btn btn-primary">Downvote</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.post-container {
+		margin-top: 10px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 10px;
+	}
+</style>
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -5,7 +5,3 @@
 
 <Navbar />
 <Main />
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-daisy:
-<button class="btn btn-error">Button</button>
