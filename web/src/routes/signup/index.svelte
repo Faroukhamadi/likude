f51b2a85814df13ab9733c5a29d98b08d8fa16ae
faroukhamadi <faroukhamadi1@outlook.com>
2022-08-04 16:19:29 +0100

    add felte

---
+++ web/src/routes/signup/index.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { createForm } from 'felte';
+
+	const { form } = createForm({
+		onSubmit: async (values) => {
+			console.log('values: ', values);
+		}
+	});
+</script>
+
+<form use:form>
+	<div class="form-control w-full max-w-xs">
+		<span class="label-text">Username</span>
+		<input
+			type="text"
+			placeholder="Enter Username"
+			name="username"
+			class="input input-bordered w-full max-w-xs"
+		/>
+	</div>
+
+	<div class="form-control w-full max-w-xs">
+		<span class="label-text">Password</span>
+		<input
+			type="password"
+			placeholder="Enter Password"
+			name="password"
+			class="input input-bordered w-full max-w-xs"
+		/>
+	</div>
+
+	<button class="btn">SignUp</button>
+</form>
