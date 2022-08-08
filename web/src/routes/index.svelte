6e555e0461b2f63361caaaa21c3a9e2d25712bd5
Faroukhamadi <faroukhamadi1@outlook.com>
2022-08-09 00:01:41 +0100

    add redirect if user is not authed in home

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -2,10 +2,22 @@
 	import Navbar from '$lib/components/home/Navbar.svelte';
 	import Main from '$lib/components/home/main.svelte';
 	import { browser } from '$app/env';
-	import { GQL_Bye, GQL_Hello } from '$houdini';
+	import { GQL_Bye, GQL_Hello, GQL_Me } from '$houdini';
+	import { goto } from '$app/navigation';
 
 	$: browser && GQL_Hello.fetch();
 	$: browser && GQL_Bye.fetch();
+	$: browser && GQL_Me.fetch();
+	$: browser && !$GQL_Me.isFetching && !$GQL_Me.data?.me && goto('/login');
+	$: browser && !$GQL_Me.isFetching && !$GQL_Me.data?.me && console.log($GQL_Me.data?.me);
+
+	if ($GQL_Me.data?.me) {
+		console.log('user is logged in');
+		console.log($GQL_Me.data?.me);
+	} else {
+		console.log('user is not logged in');
+		console.log($GQL_Me.data?.me);
+	}
 </script>
 
 {#if $GQL_Hello.isFetching}
@@ -21,6 +33,16 @@
 {:else if $GQL_Bye.data}
 	<h1>This is the bye query: {$GQL_Bye.data?.bye}</h1>
 {:else}
+	<h1>Loading...</h1>
+{/if}
+
+{#if $GQL_Me.isFetching}
+	<h1>Me query is loading</h1>
+{:else if $GQL_Me.data}
+	<h1>This is the id: {$GQL_Me.data?.me?.id}</h1>
+	<h1>This is the username: {$GQL_Me.data?.me?.username}</h1>
+{:else}
+	<h1>Loading...</h1>
 	<h1>Loading...</h1>
 {/if}
 
